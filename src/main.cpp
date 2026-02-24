6ea1b4829506f188cd03e6429aa71950c300d4f5
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-02-24 15:23:25 +0000

    Configuration constants

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,25 @@
 #include <bn_color.h>
 #include <bn_keypad.h>
 
+// Width and height of the the player bounding box
+static constexpr bn::size PLAYER_SIZE = {8, 8};
+
+static constexpr int MIN_Y = -bn::display::height() / 2;
+static constexpr int MAX_Y = bn::display::height() / 2;
+static constexpr int MIN_X = -bn::display::width() / 2;
+static constexpr int MAX_X = bn::display::width() / 2;
+
+// Number of characters required to show the longest numer possible in an int (-2147483647)
+static constexpr int MAX_SCORE_CHARS = 11;
+
+// Score location
+static constexpr int SCORE_X = 70;
+static constexpr int SCORE_Y = -70;
+
+// High score location
+static constexpr int HIGH_SCORE_X = -70;
+static constexpr int HIGH_SCORE_Y = -70;
+
 int main() {
     bn::core::init();
 
