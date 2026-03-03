0d77db75428d86cb2e87020e2f42cd3a81a8c1bc
kelley <kelleycastillo@outlook.com>
2026-03-03 04:51:24 +0000

    added powerup and util

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,10 @@
+#include "utils.h"
+#include "Player.h"
+#include "Enemy.h"
+#include "PowerupType.h"
+#include "ScoreDisplay.h"
+#include "utils.h"
+
 #include <bn_core.h>
 #include <bn_display.h>
 #include <bn_keypad.h>
@@ -53,12 +60,12 @@ static constexpr int ENEMY_SEPARATION = 14;
  * sprite the sprite to center the box around
  * box_size the dimensions of the bounding box
  */
-bn::rect create_bounding_box(bn::sprite_ptr sprite, bn::size box_size) {
-    return bn::rect(sprite.x().round_integer(),
-                    sprite.y().round_integer(),
-                    box_size.width(),
-                    box_size.height());
-}
+// bn::rect create_bounding_box(bn::sprite_ptr sprite, bn::size box_size) {
+//     return bn::rect(sprite.x().round_integer(),
+//                     sprite.y().round_integer(),
+//                     box_size.width(),
+//                     box_size.height());
+// }
 
 /**
  * Displays a score and high score.
