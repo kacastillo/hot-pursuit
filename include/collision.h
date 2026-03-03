851a60464cd8040c10384581e0b6bcc446808c08
Kate_Nguyen <Kate_nguyen>
2026-03-03 04:52:34 +0000

    move create_bounding_box to collision file cpp and h

---
+++ include/collision.h
@@ -0,0 +1,10 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include <bn_rect.h>
+#include <bn_sprite_ptr.h>
+#include <bn_size.h>
+
+bn::rect create_bounding_box(bn::sprite_ptr sprite, bn::size box_size);
+
+#endif
---
+++ src/collision.cpp
@@ -0,0 +1,14 @@
+#include "collision.h"
+
+
+/**
+ * Creates a rectangle centered at a sprite's location with a given size.
+ * sprite the sprite to center the box around
+ * box_size the dimensions of the bounding box
+ */
+bn::rect create_bounding_box(bn::sprite_ptr sprite, bn::size box_size) {
+    return bn::rect(sprite.x().round_integer(),
+                    sprite.y().round_integer(),
+                    box_size.width(),
+                    box_size.height());
+}
